0685ad7e65f7b9d26376fbb2b0463d56dee50441
phoenix9419 <dhruvikb94@gmail.com>
2024-07-04 15:32:31 +0530

    docker url changes

--- frontend/src/apps/EvenmoreOs.jsx
+++ frontend/src/apps/EvenmoreOs.jsx
@@ -20,11 +20,11 @@ const DefaultApp = () => (
   </Localization>
 );
 
-export default function IdurarOs() {
+export default function EvenmoreOs() {
   const { isLoggedIn } = useSelector(selectAuth);
 
   console.log(
-    '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
+    '🚀 Welcome to EVENMORE ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@evenmoreapp.com for more information.'
   );
 
   // // Online state
